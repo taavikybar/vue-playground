48a8582c51d6923dfab11f271e42c80076e0b054
Taavi Kybar <taavi.kubar@pipedrive.com>
2022-12-09 14:08:20 +0200

    initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {ref, onMounted} from 'vue'
+import {useCounterStore} from '../stores/counter'
+
+const counter = useCounterStore()
+
+let count = ref(0)
+
+const increment = () => count.value++
+
+onMounted(() => {
+  console.log(`initial count ${count.value}`, counter)
+})
+
+
+</script>
+
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="increment">count is {{count}}</button>
+    <button @click="counter.increment">counter is {{counter.count}}</button>
+    <button>{{counter.doubleCount}}</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
